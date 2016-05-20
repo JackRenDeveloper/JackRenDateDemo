c58397cc4654884a243beddf9a9b20dcdd93d8e0
JackRenDeveloper <jackren_developer@163.com>
2016-05-20 10:00:07 -0700

    Initial Commit

---
+++ JackRenDateDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JackRenDateDemo
+//
+//  Created by JackRen on 16/5/20.
+//  Copyright © 2016年 JackRen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
